97a190a3ef14d74efc3f2c6399e2b242cd28174d
Muhammad Umar <umarmachrani55@gmail.com>
2026-04-21 14:07:37 +0500

    Allow all origins for demo

--- src/index.js
+++ src/index.js
@@ -11,11 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "http://localhost:3000",
-    ],
+    origin: "*",  
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
   })
